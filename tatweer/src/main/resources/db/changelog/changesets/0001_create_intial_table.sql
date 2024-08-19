ab506ce1f5fa78b10ad8d5a1dc2682ef7c4b1fdb
Ahmed Marzook <m4marzook@gmail.com>
2024-08-19 23:23:25 +0100

    Postgres Database Setup

--- tatweer/src/main/resources/db/changelog/changesets/0001_create_intial_table.sql
+++ tatweer/src/main/resources/db/changelog/changesets/0001_create_intial_table.sql
@@ -1,13 +1,14 @@
 --liquibase formatted SQL
+
 --changeset ahmedM:V0001
 --comment: creating a user table
 
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    firstName VARCHAR(50) NOT NULL,
-    lastName VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     updated_at TIMESTAMP DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
